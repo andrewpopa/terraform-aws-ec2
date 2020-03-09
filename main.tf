7b0abe46a7132efaff935beba8dc417623e4d610
Andrei Popa <andrei@hashicorp.com>
2020-03-09 13:58:54 +0100

    key name from external module

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 # EC2
-resource "aws_key_pair" "tf_key" {
-  key_name   = var.key_name
-  public_key = var.public_key
-}
-
 resource "aws_instance" "tf_ec2" {
   ami                         = var.ami_type
   instance_type               = var.ec2_instance["type"]
   vpc_security_group_ids      = [var.vpc_security_group_ids]
-  key_name                    = aws_key_pair.tf_key.key_name
+  key_name                    = var.key_name
   subnet_id                   = var.subnet_id
   associate_public_ip_address = var.public_ip
   user_data                   = var.user_data
